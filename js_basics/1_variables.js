ee0c7093755e5369df077a19d7b9884463dc0dd5
shobham17 <shobhamkishan17@gmail.com>
2025-06-09 15:50:03 +0000

    container config done

---
+++ js_basics/1_variables.js
@@ -0,0 +1,15 @@
+const accountid = 31313
+let accountmail = "shobhamkishan17@gmail.com"
+var acccountpass = "12143"
+accountcity = "bilaspur"
+
+//accountid  = 2 NOT ALLOWED 
+
+accountmail = "hchc.com"
+accountpass = "1123123"
+accountcity = "banglore"
+
+console.log(accountid)
+console.table([accountid, accountmail, accountpass, accountcity])
+
+//mot to use var coz of issue in block scope and funtional scope
