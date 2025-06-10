60ec2ebb00925328d1a469b6469fbe008f551fa9
shobham17 <shobhamkishan17@gmail.com>
2025-06-10 06:22:28 +0000

    conversion of datatypes

---
+++ js_basics/3_conversionoperations.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueinnumber = Number(score)// converts the string value into number
+console.log(typeof valueinnumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let loggedin = 1;
+let boollogged = Boolean(loggedin)
+console.log(boollogged)
+
+//if here the string is been used and if its empty then bool is false
+//if anything is written in the string the returns true
+
+// 1 => true; 0 => false;
+// "" => false
+// "shobham" => true
+
+let somenum = 33
+let somestr = string(somenum)
+console.log(somestr)
+console.log(typeof somestr)
