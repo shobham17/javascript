115e5697054b5e28ee3f412f027aea883d6e563b
shobham17 <shobhamkishan17@gmail.com>
2025-06-10 14:21:07 +0000

    comparison

--- js_basics/4_comparison.js
+++ js_basics/4_comparison.js
@@ -21,3 +21,24 @@ console.log("2" === 2)
 const myfunction = function(){
     console.log("hello world")
 }
+
+//******************************* */
+// STACK(primitive) , HEAP(non primitive)
+
+let myname = "shobham kishan"
+let anothername = myname
+anothername = "nikhil"
+console.log(myname)
+console.log(anothername)
+
+let userone = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let usertwo = userone
+usertwo.email = "sk@google.com"
+
+console.log(userone.email)
+console.log(usertwo.email)
+// tgis both gives same output as sk@google.com
