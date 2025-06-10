f6b7398244b7c825cd436db9020b7f6d4ff81bbe
shobham17 <shobhamkishan17@gmail.com>
2025-06-10 05:34:24 +0000

    datatypes

---
+++ js_basics/2_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // terate all js code as newer version
+
+//alert("hello") // wwe are using nodejs not browser
+
+console.log(3
+    +
+    3)// code readabilityis less
+
+    let name = "shobham" //string
+    let age = 19//number
+    let isloggein = false //boolean
+
+    //number => 2 to power 53
+// // bigint
+// // string => ""
+// //boolean => true or false
+// // null => standalone value
+// //undefined =>
+// //symbol => unique
+
+//to find out the type of datatype
+console.log(typeof "shobham")
+console.log(typeof null)
