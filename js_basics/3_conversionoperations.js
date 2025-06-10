33f137419ba61f337f770d18789f1144963f8d9c
shobham17 <shobhamkishan17@gmail.com>
2025-06-10 06:37:29 +0000

    conversion on operations

--- js_basics/3_conversionoperations.js
+++ js_basics/3_conversionoperations.js
@@ -26,3 +26,34 @@ let somenum = 33
 let somestr = string(somenum)
 console.log(somestr)
 console.log(typeof somestr)
+
+//***************OPERATIONS************ */
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// basic mathematical functions
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = "shobham"
+let str3 = str1 + str2
+console.log(str3)
+// this will concatenate the string
+
+console.log("1"+2) // gives output 12
+console.log(1+"2") // gives output 12
+console.log("1"+2+2) // gives output 122 all cobnversion is in string
+
+console.log(1+2+"2")
+// this gives output 32
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
