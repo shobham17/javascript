1f3575dab12c54ef273ab3bc4bf6610493eef638
shobham17 <shobhamkishan17@gmail.com>
2025-06-10 12:48:47 +0000

    comparison

---
+++ js_basics/4_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// this all will give boolean output 
+
+// console.log("2">1)
+// console.log("02">1)
+
+console.log(null>0) //false
+console.log(null==0) // false
+console.log(null>=0) //true
+
+// === this is called strict check
+console.log("2" === 2)
+//here it does not convert and compare
