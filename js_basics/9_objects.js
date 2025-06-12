2538706cf3bde6728342a2eee79e3ac3bacfc784
shobham17 <shobhamkishan17@gmail.com>
2025-06-12 12:36:09 +0000

    objects

--- js_basics/9_objects.js
+++ js_basics/9_objects.js
@@ -29,3 +29,40 @@ jsuser.greetingstwo = function(){
     console.log(`hello js user,${this.name}`)
 }
 console.log(jsuser.greetingstwo)
+
+//*********OBJECTS PART2********** */
+// const tinderuser = new Object()
+const tinderuser = {}
+tinderuser.id = "123abc"
+tinderuser.name ="shobham"
+tinderuser.isloggedin = false
+
+const regularuser = {
+    email : "shobham@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "shobham",
+            lastname : "kishan"
+        }
+    }
+}
+console.log(regularuser.fullname) 
+console.log(regularuser.fullname.userfullname.firstname)
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj3 = Object.assign(obj1, obj2)
+
+const users =  [
+    {
+        id  : 1,
+        email : "sk@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderuser)
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
