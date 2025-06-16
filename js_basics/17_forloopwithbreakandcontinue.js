d381f6ba49d22f2095c383faa01bed9ec277c356
shobham17 <shobhamkishan17@gmail.com>
2025-06-16 05:21:57 +0000

    for loop

---
+++ js_basics/17_forloopwithbreakandcontinue.js
@@ -0,0 +1,23 @@
+// FOR LOOP
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
+}
+
+let myarr = ["flash", "batman", "supaerman"]
+console.log(myarr.length)
+    for(let i = 0; i<myarr; i++){
+        const elemnt = myarr[i]
+        console.log(element)
+    }
+
+
+// BREAK AND CONTINUE
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log("detected 5")
+        break;
+    }
+    console.log(index)
+}
