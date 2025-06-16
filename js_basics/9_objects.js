1090c3dfd537420b62442fa075111082c3ada841
shobham17 <shobhamkishan17@gmail.com>
2025-06-16 05:35:07 +0000

    objects

--- js_basics/9_objects.js
+++ js_basics/9_objects.js
@@ -1,92 +1,123 @@
-//SINGLETON
+// SINGLETON - Creating an object (singleton example would typically use constructors)
 
-//OBJECT LITERALS
-const mysym=  Symbol("key1")
-const jsuser= {
-    name : "shobham",
+// OBJECT LITERALS
+const mysym = Symbol("key1");
+
+const jsuser = {
+    name: "shobham",
     [mysym]: "mykey1",
-    age  : 19,
-    location : "bilaspur",
-    email : "sk@gmail.com",
-    isloggedin : false,
-    lastlogindays : ["monday",  "saturday"]
-}
-console.log(jsuser.email)
-console.log(jsuser["email"])
-console.log(jsuser[mysym])
+    age: 19,
+    location: "bilaspur",
+    email: "sk@gmail.com",
+    isloggedin: false,
+    lastlogindays: ["monday", "saturday"]
+};
 
-jsuser.email =  "nikhil@gmail.com"
-//change email
-Object.freeze(jsuser)
+console.log(jsuser.email);         // Access via dot
+console.log(jsuser["email"]);      // Access via bracket
+console.log(jsuser[mysym]);        // Access Symbol property
 
+// Modify email
+jsuser.email = "nikhil@gmail.com";
 
-jsuser.greeting=function(){
-    console.log("hello js user")
-}
-console.log(jsuser.greeting())
+// Freeze the object
+Object.freeze(jsuser);
 
-jsuser.greetingstwo = function(){
-    console.log(`hello js user,${this.name}`)
-}
-console.log(jsuser.greetingstwo)
+// These lines will not work because jsuser is frozen
+// jsuser.greeting = function () {
+//     console.log("hello js user");
+// };
+// jsuser.greetingstwo = function () {
+//     console.log(`hello js user, ${this.name}`);
+// };
 
-//*********OBJECTS PART2********** */
-// const tinderuser = new Object()
-const tinderuser = {}
-tinderuser.id = "123abc"
-tinderuser.name ="shobham"
-tinderuser.isloggedin = false
+// To demonstrate greeting, define before freeze:
+const jsuser2 = {
+    name: "shobham",
+    greeting() {
+        console.log("hello js user");
+    },
+    greetingstwo() {
+        console.log(`hello js user, ${this.name}`);
+    }
+};
+
+jsuser2.greeting();         // hello js user
+jsuser2.greetingstwo();     // hello js user, shobham
+
+// OBJECTS PART 2
+
+// Create an object (non-singleton)
+const tinderuser = {};
+tinderuser.id = "123abc";
+tinderuser.name = "shobham";
+tinderuser.isloggedin = false;
 
 const regularuser = {
-    email : "shobham@gmail.com",
-    fullname : {
-        userfullname : {
-            firstname : "shobham",
-            lastname : "kishan"
+    email: "shobham@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shobham",
+            lastname: "kishan"
         }
     }
-}
-console.log(regularuser.fullname) 
-console.log(regularuser.fullname.userfullname.firstname)
+};
 
+console.log(regularuser.fullname);
+console.log(regularuser.fullname.userfullname.firstname);
 
-const obj1 = {1:"a", 2:"b"}
-const obj2 = {3:"a", 4:"b"}
-const obj3 = Object.assign(obj1, obj2)
+// Object merging
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+const obj3 = Object.assign({}, obj1, obj2); // avoid mutating obj1
 
-const users =  [
+console.log(obj3); // {1: "a", 2: "b", 3: "c", 4: "d"}
+
+// Array of users
+const users = [
     {
-        id  : 1,
-        email : "sk@gmail.com"
+        id: 1,
+        email: "sk@gmail.com"
     }
-]
+];
 
-users[1].email
-console.log(tinderuser)
-console.log(Object.keys(tinderuser))
-console.log(Object.values(tinderuser))
-console.log(Object.entries(tinderuser))
+// Safely access users
+if (users[1]) {
+    console.log(users[1].email);
+} else {
+    console.log("User at index 1 does not exist.");
+}
 
+// Tinder user info
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));       // Keys as array
+console.log(Object.values(tinderuser));     // Values as array
+console.log(Object.entries(tinderuser));    // Entries as [key, value]
+
+// Destructuring
 const course = {
-    coursename : "js in hindi",
-    price : "999",
-    courseinstructor : "shobham"
-}
+    coursename: "js in hindi",
+    price: "999",
+    courseinstructor: "shobham"
+};
 
-const {courseinstructor : instructor } = course
-console.log(instructor)
+const { courseinstructor: instructor } = course;
+console.log(instructor); // shobham
 
+// Correct Object Declaration
+const courseInfo = {
+    name: "shobham",
+    courseaname: "js in hindi",
+    prize: "free"
+};
 
+console.log(courseInfo);
 
-{
-    name =  "shobham",
-    "courseaname"  :   "js in hindi",
-    "prize" :  "free"
-}
+// Valid array of objects structure
+const courseArray = [
+    { id: 1, name: "HTML" },
+    { id: 2, name: "CSS" },
+    { id: 3, name: "JavaScript" }
+];
 
-[
-    {},
-    {},
-    {}
-]
-
+console.log(courseArray);
